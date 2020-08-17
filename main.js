48f15dca7462441d080dcc2d94a375b7ad79e8ec
joyoanta <banik.joyoanta12@gmail.com>
2020-08-17 18:32:28 +0600

    song finding done

--- main.js
+++ main.js
@@ -1,30 +1,91 @@
-document.getElementById('search-btn').addEventListener("click",function(){
-   const searchData = document.getElementById('search-data').value ; 
+const form = document.getElementById('form'); 
+const search = document.getElementById('search'); 
+const result = document.getElementById('result'); 
+const more = document.getElementById('more'); 
 
-   if (searchData == "" || searchData == null){
+// event listeners
 
-      alert ('Please Enter a song name')
+form.addEventListener('submit', function(e) {
+   e.preventDefault(); 
+   const searchTerm = search.value ; 
+   
+   if(searchTerm == ''|| searchTerm == null){
+      alert (" Plaeas Enter a song name !! ")
    }
-   else {
-      findLyrics(searchData);
-      // console.log("pressed")
+   else{
+      searchSongs (searchTerm); 
    }
 
 })
 
-function findLyrics(song) {
-   const searchBtn = document.getElementById('search-btn').value ; 
-   fetch(`https://api.lyrics.ovh/suggest${song}`)
-   .then(response => response.json())
-   .then (data => {
+async function searchSongs(term) {
+   const res = await fetch(`https://api.lyrics.ovh/suggest/${term}&limit=10`);
+    const data = await res.json();
+  
+    showData(data);
 
-      let songName ; 
-      let artistName; 
-
-   })
+    console.log(data)
+ }
 
 
 
+function showData(data){
 
-
+   let output = '';
+   data.data.forEach(song => {
+      output += ` 
+      
+      <li> 
+         <span><strong> ${song.title} </strong> Album by  ${song.artist.name} </span>
+         <button class="btn btn-success" data-artist= "${song.artist.name}" data-songtitle = "${song.title}">Get Lyrics</button>
+      </li>
+      `;
+   });
+   
+   result.innerHTML = `
+   <ul class="songs">
+      ${output}
+   </ul>
+   `;
+   
 }
+
+
+async function getLyrics(songTitle, artist){
+
+   const res = await fetch(`https://api.lyrics.ovh/v1/${artist}/${songTitle}`);
+   const data = await res.json();
+
+  const lyrics = data.lyrics.replace(/(\r\n|\r |\n)/g,"<br>");
+  result.innerHTML = `<h2> ${songTitle}- ${artist} </h2>
+  <span> ${lyrics} </span>`
+}
+
+// Lyrics Button 
+
+result.addEventListener ("click", function(e){
+
+   const buttonPress = e.target;
+
+   if (buttonPress.tagName === "BUTTON" ){
+
+      const songTitle =  buttonPress.getAttribute ('data-songtitle');
+      const artist =  buttonPress.getAttribute ('data-artist');
+
+      getLyrics(songTitle, artist);
+   }
+})
+
+
+
+
+   // function searchSongs (term){
+
+   //    fetch (`https://api.lyrics.ovh/suggest/${term}`)
+   //    .then (res => res.json())
+   //    .then (data => console.log(data));
+   
+   //    showData (data); 
+
+    
+   
